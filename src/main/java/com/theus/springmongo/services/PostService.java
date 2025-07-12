30f18e090a978ca129c9d58823c0be4f7015e260
Matheus-Rocha-Silva <matheusilva2244@gmail.com>
2025-07-12 16:33:59 -0300

    Obtendo um post por Id

---
+++ src/main/java/com/theus/springmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.theus.springmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.theus.springmongo.domain.Post;
+import com.theus.springmongo.repository.PostRepository;
+import com.theus.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post user = repo.findById(id);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}
+		return user;
+	}
+	
+}
