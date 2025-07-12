6a11547cdb36ea4ad86f10e21852154999360b77
Matheus-Rocha-Silva <matheusilva2244@gmail.com>
2025-07-12 17:26:16 -0300

    Consulta simples com query methods

--- src/main/java/com/theus/springmongo/services/PostService.java
+++ src/main/java/com/theus/springmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.theus.springmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,4 +23,7 @@ public class PostService {
 		return user;
 	}
 	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
